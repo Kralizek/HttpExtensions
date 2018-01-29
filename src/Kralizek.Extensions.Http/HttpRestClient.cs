18153e62014e84881f8a759aae79076a309a186c
Renato Golia <golia.renato@gmail.com>
2018-01-29 01:16:49 +0100

    Added HttpRestClient

---
+++ src/Kralizek.Extensions.Http/HttpRestClient.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Kralizek.Extensions.Http
+{
+    public class HttpRestClient
+    {
+        private readonly HttpClient _client;
+        private readonly JsonSerializerSettings _serializerSettings;
+        private readonly ILogger _logger;
+
+        public HttpRestClient(HttpClient client, JsonSerializerSettings serializerSettings, ILogger logger)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+            _serializerSettings = serializerSettings ?? throw new ArgumentNullException(nameof(serializerSettings));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        private static string ComposeUrl(string url, IQueryString query)
+        {
+            if (query != null && query.HasItems)
+                return $"{url}?{query.Query}";
+
+            return url;
+        }
+
+        #region Send
+
+        public async Task<TResult> SendAsync<TContent, TResult>(HttpMethod method, string url, TContent content, IQueryString query = null)
+        {
+            var json = JsonConvert.SerializeObject(content, _serializerSettings);
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var requestUrl = ComposeUrl(url, query);
+
+            using (var request = new HttpRequestMessage(method, requestUrl) { Content = httpContent })
+            {
+                await LogRequest(request, includeContent: true);
+
+                using (var response = await _client.SendAsync(request).ConfigureAwait(false))
+                {
+                    await LogResponse(response);
+
+                    response.EnsureSuccessStatusCode();
+
+                    var incomingContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                    var result = JsonConvert.DeserializeObject<TResult>(incomingContent, _serializerSettings);
+
+                    return result;
+                }
+            }
+        }
+
+        public async Task<TResult> SendAsync<TResult>(HttpMethod method, string url, IQueryString query = null)
+        {
+            var requestUrl = ComposeUrl(url, query);
+
+            using (var request = new HttpRequestMessage(method, requestUrl))
+            {
+                await LogRequest(request);
+
+                using (var response = await _client.SendAsync(request).ConfigureAwait(false))
+                {
+                    await LogResponse(response);
+
+                    response.EnsureSuccessStatusCode();
+
+                    var incomingContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                    var result = JsonConvert.DeserializeObject<TResult>(incomingContent, _serializerSettings);
+
+                    return result;
+                }
+            }
+        }
+
+        public async Task SendAsync<TContent>(HttpMethod method, string url, TContent content, IQueryString query = null)
+        {
+            var json = JsonConvert.SerializeObject(content, _serializerSettings);
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var requestUrl = ComposeUrl(url, query);
+
+            using (var request = new HttpRequestMessage(method, requestUrl) { Content = httpContent })
+            {
+                await LogRequest(request, includeContent: true);
+
+                using (var response = await _client.SendAsync(request).ConfigureAwait(false))
+                {
+                    await LogResponse(response);
+
+                    response.EnsureSuccessStatusCode();
+                }
+            }
+        }
+
+        public async Task SendAsync(HttpMethod method, string url, IQueryString query = null)
+        {
+            var requestUrl = ComposeUrl(url, query);
+
+            using (var request = new HttpRequestMessage(method, requestUrl))
+            {
+                await LogRequest(request, includeContent: true);
+
+                using (var response = await _client.SendAsync(request).ConfigureAwait(false))
+                {
+                    await LogResponse(response);
+
+                    response.EnsureSuccessStatusCode();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Logging
+
+        private static readonly IReadOnlyDictionary<HttpMethod, EventId> HttpMethodEventIds = new Dictionary<HttpMethod, EventId>
+        {
+            [HttpMethod.Get] = new EventId(1001, HttpMethod.Get.Method),
+            [HttpMethod.Post] = new EventId(1002, HttpMethod.Post.Method),
+            [HttpMethod.Put] = new EventId(1003, HttpMethod.Put.Method),
+            [HttpMethod.Delete] = new EventId(1004, HttpMethod.Delete.Method)
+        };
+
+        private async Task LogResponse(HttpResponseMessage response)
+        {
+            var eventId = new EventId((int)response.StatusCode, response.ReasonPhrase);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                try
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                    var state = new
+                    {
+                        method = response.RequestMessage.Method.Method.ToUpper(),
+                        requestUri = response.RequestMessage.RequestUri,
+                        status = response.StatusCode,
+                        reasonPhrase = response.ReasonPhrase,
+                        errorMessage = responseContent
+                    };
+
+                    _logger.LogError(eventId, state, s => $"{s.method}: {s.requestUri.PathAndQuery} {s.status:D} '{s.reasonPhrase}' '{s.errorMessage}'");
+                }
+                catch (Exception ex)
+                {
+                    var state = new
+                    {
+                        method = response.RequestMessage.Method.Method.ToUpper(),
+                        requestUri = response.RequestMessage.RequestUri,
+                        status = response.StatusCode,
+                        reasonPhrase = response.ReasonPhrase
+                    };
+
+                    _logger.LogError(eventId, state, ex, (s, e) => $"{s.method}: {s.requestUri.PathAndQuery} {s.status:D} '{s.reasonPhrase}' '{e.Message}'");
+                }
+            }
+            else
+            {
+                var state = new
+                {
+                    method = response.RequestMessage.Method.Method.ToUpper(),
+                    requestUri = response.RequestMessage.RequestUri,
+                    status = response.StatusCode,
+                    reasonPhrase = response.ReasonPhrase
+                };
+
+                _logger.LogDebug(eventId, state, s => $"{s.method}: {s.requestUri.PathAndQuery} {s.status:D} '{s.reasonPhrase}'");
+            }
+
+        }
+
+        private async Task LogRequest(HttpRequestMessage request, bool includeContent = false)
+        {
+            var eventId = HttpMethodEventIds[request.Method];
+
+            var state = new
+            {
+                method = request.Method.Method.ToUpper(),
+                requestUri = request.RequestUri,
+                content = includeContent ? await (request.Content?.ReadAsStringAsync() ?? Task.FromResult((string)null)) : null,
+                contentType = request.Content?.GetType().Name
+            };
+
+            _logger.LogDebug(eventId, state, s => $"{s.method}: {s.requestUri} {(includeContent ? s.content : s.contentType)}");
+
+        }
+
+        #endregion
+    }
+}
---
+++ tests/Tests.Extensions.Http/HttpRestClientTests.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AutoFixture;
+using AutoFixture.NUnit3;
+using Kralizek.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using WorldDomination.Net.Http;
+
+namespace Tests.Extensions.Http
+{
+    [TestFixture]
+    public class HttpRestClientTests
+    {
+        private IFixture fixture;
+        private JsonSerializerSettings serializerSettings;
+
+        [SetUp]
+        public void Initialize()
+        {
+            fixture = new Fixture();
+            serializerSettings = new JsonSerializerSettings();
+        }
+
+        [Test]
+        public void HttpClient_cant_be_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new HttpRestClient(null, new JsonSerializerSettings(), Mock.Of<ILogger>()));
+        }
+
+        [Test]
+        public void JsonSerializerSettings_cant_be_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new HttpRestClient(Mock.Of<HttpClient>(), null, Mock.Of<ILogger>()));
+        }
+
+        [Test]
+        public void Logger_cant_be_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new HttpRestClient(Mock.Of<HttpClient>(), new JsonSerializerSettings(), null));
+        }
+
+        private HttpRestClient CreateSystemUnderTest(params HttpMessageOptions[] options)
+        {
+            var messageHandler = new FakeHttpMessageHandler(options);
+            var httpClient = new HttpClient(messageHandler);
+            return new HttpRestClient(httpClient, serializerSettings, Mock.Of<ILogger>());
+        }
+
+        [Test]
+        [InlineAutoData("GET")]
+        [InlineAutoData("POST")]
+        [InlineAutoData("PUT")]
+        [InlineAutoData("DELETE")]
+        public async Task SendAsync_can_send_request_with_body_and_receive_response_with_body(string method, Uri uri, Request request, Response response)
+        {
+            var httpMethod = new HttpMethod(method);
+
+            var options = new[]
+            {
+                new HttpMessageOptions
+                {
+                    RequestUri = uri,
+                    HttpMethod = httpMethod,
+                    HttpContent = new StringContent(JsonConvert.SerializeObject(request, serializerSettings)),
+                    HttpResponseMessage = new HttpResponseMessage
+                    {
+                        Content = new StringContent(JsonConvert.SerializeObject(response, serializerSettings))
+                    }
+                }
+            };
+            var sut = CreateSystemUnderTest(options);
+
+            var actualResponse = await sut.SendAsync<Request, Response>(httpMethod, uri.ToString(), request);
+
+            Assert.That(actualResponse, Is.EqualTo(response));
+        }
+
+        [Test]
+        [InlineAutoData("GET")]
+        [InlineAutoData("POST")]
+        [InlineAutoData("PUT")]
+        [InlineAutoData("DELETE")]
+        public async Task SendAsync_can_send_request_with_body_and_receive_response(string method, Uri uri, Request request)
+        {
+            var httpMethod = new HttpMethod(method);
+
+            var options = new[]
+            {
+                new HttpMessageOptions
+                {
+                    RequestUri = uri,
+                    HttpMethod = httpMethod,
+                    HttpContent = new StringContent(JsonConvert.SerializeObject(request, serializerSettings)),
+                    HttpResponseMessage = new HttpResponseMessage(HttpStatusCode.OK)
+                }
+            };
+            var sut = CreateSystemUnderTest(options);
+
+            await sut.SendAsync(httpMethod, uri.ToString(), request);
+        }
+
+        [Test]
+        [InlineAutoData("GET")]
+        [InlineAutoData("POST")]
+        [InlineAutoData("PUT")]
+        [InlineAutoData("DELETE")]
+        public async Task SendAsync_can_send_request_and_receive_response_with_body(string method, Uri uri, Response response)
+        {
+            var httpMethod = new HttpMethod(method);
+
+            var options = new[]
+            {
+                new HttpMessageOptions
+                {
+                    RequestUri = uri,
+                    HttpMethod = httpMethod,
+                    HttpResponseMessage = new HttpResponseMessage
+                    {
+                        Content = new StringContent(JsonConvert.SerializeObject(response, serializerSettings))
+                    }
+                }
+            };
+            var sut = CreateSystemUnderTest(options);
+
+            var actualResponse = await sut.SendAsync<Response>(httpMethod, uri.ToString());
+
+            Assert.That(actualResponse, Is.EqualTo(response));
+        }
+
+        [Test]
+        [InlineAutoData("GET")]
+        [InlineAutoData("POST")]
+        [InlineAutoData("PUT")]
+        [InlineAutoData("DELETE")]
+        public async Task SendAsync_can_send_request_and_receive_response(string method, Uri uri)
+        {
+            var httpMethod = new HttpMethod(method);
+
+            var options = new[]
+            {
+                new HttpMessageOptions
+                {
+                    RequestUri = uri,
+                    HttpMethod = httpMethod,
+                    HttpResponseMessage = new HttpResponseMessage(HttpStatusCode.OK)
+                }
+            };
+            var sut = CreateSystemUnderTest(options);
+
+            await sut.SendAsync(httpMethod, uri.ToString());
+        }
+
+
+        public class Request
+        {
+            public int IntValue { get; set; }
+
+            public string StringValue { get; set; }
+
+            public DateTimeOffset DateTimeOffsetValue { get; set; }
+        }
+
+        public class Response : IEquatable<Response>
+        {
+            public int IntValue { get; set; }
+
+            public string StringValue { get; set; }
+
+            public DateTimeOffset DateTimeOffsetValue { get; set; }
+
+            bool IEquatable<Response>.Equals(Response other)
+            {
+                if (other == null)
+                    return false;
+
+                if (IntValue != other.IntValue)
+                    return false;
+
+                if (StringValue != other.StringValue)
+                    return false;
+
+                if (DateTimeOffsetValue != other.DateTimeOffsetValue)
+                    return false;
+
+                return true;
+            }
+        }
+
+    }
+}
